330fabc88577772d35b2e605eb4e632bcd75b01b
Sorryjjj <1528164110@qq.com>
2021-08-29 12:22:48 +0800

    feat: add vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import Web3 from 'web3'
@@ -12,5 +13,6 @@ const web3 = new Web3(Web3.givenProvider || 'ws://localhost:8545')
 Vue.prototype.web3 = web3
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
